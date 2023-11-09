64b7087f9dfee895cc2c34b0cb0a4f6286978164
Judice <r_judice@msn.com>
2023-11-08 21:14:09 -0300

    Estrutura Inicial

---
+++ src/test/java/com/git/judice/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.judice.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
